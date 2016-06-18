5bf8a6e64a3ad437e05eb557270614d2615b202d
Jake Sendar <jakesendar@gmail.com>
2016-06-17 22:21:02 -0400

    move params fn inside register fn

--- src/plugins/url/register.js
+++ src/plugins/url/register.js
@@ -1,27 +1,33 @@
 import routes from './routes';
 
-const urlSearchToObj = (search) => {
-  const str = search.split('?')[1];
-  const obj = str && str
-    .split('&')
-    .reduce((curr, p) => {
-      const [key, val] = p.split('=');
-
-      curr[key] = val;
-
-      return curr;
-    });
-
-  return obj || {};
-};
-
 export default function register (router) {
+  // converts url search string to js object
+  const urlSearchToObj = (search) => {
+    const str = search.split('?')[1];
+    const obj = str && str
+      .split('&')
+      .reduce((curr, p) => {
+        const [key, val] = p.split('=');
+
+        curr[key] = val;
+
+        return curr;
+      });
+
+    return obj || {};
+  };
+
+
   const onUrlChange = (e = {}) => {
     const state = e.state || {},
+      // state.payload comes from
+      // corresponding url event
       payload = state.payload || {},
       { pathname, search } = window.location,
       params = urlSearchToObj(search);
 
+    // trigger url event if current
+    // pathname matches a url route
     for (let key in routes) {
       if (pathname === key) {
         router.route({
@@ -36,9 +42,12 @@ export default function register (router) {
     };
   };
 
+  // TODO: add payload to url search params
   // on new route, add to pushState to change url
   router.on('route', ({ type, key, payload }) => {
-    // check pathname to prevent triggering twice
+    // if pathname matches key then the url event
+    // was already triggered via onUrlChange, so
+    // do nothing
     if (type === 'url' && window.location.pathname !== key) {
       window.history.pushState({ payload }, '', key);
     };
